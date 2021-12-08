aa01c1b7e1d78e3a3421d5382721f882a391e0ed
yuanqingtao <yqt8926@163.com>
2021-12-08 11:12:21 +0800

    Initial commit

---
+++ src/main/java/utilities/ObjectsTest.java
@@ -0,0 +1,28 @@
+package utilities;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Objects;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author yuanqingtao
+ * @version 1.0
+ * @date 2021/12/8 10:39 上午
+ * @description
+ */
+//@Log4j2
+////@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = BaseTestConfiguration.class)
+
+public class ObjectsTest {
+    public static void main(String[] args) {
+        if (Objects.equal(1,2)){
+
+        }
+//        Joiner.on("$").
+//        Function<>
+    }
+}
---
+++ src/test/java/utilities/ObjectsTest.java
@@ -0,0 +1,20 @@
+package utilities;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Objects;
+import org.junit.Test;
+
+/**
+ * @author yuanqingtao
+ * @version 1.0
+ * @date 2021/12/8 10:39 上午
+ * @description
+ */
+public class ObjectsTest {
+    @Test
+    public void test(){
+        System.out.println("单元测试");
+    }
+
+}
